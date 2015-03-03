da35f2ca88ceffcd8f37e819bac151f8a50f1297
ylcheungab <ylcheungab@connect.ust.hk>
2015-03-03 16:53:57 +0800

    generate creature, item, map

---
+++ TowerOfSorcerer/src/creatures/creatures.java
@@ -0,0 +1,42 @@
+package creatures;
+
+public class creatures {
+	private String name;
+	private int ID;
+	private int hp;
+	private int atk;
+	private int def;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getAtk() {
+		return atk;
+	}
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+	public int getDef() {
+		return def;
+	}
+	public void setDef(int def) {
+		this.def = def;
+	}
+	
+	
+}
---
+++ TowerOfSorcerer/src/items/items.java
@@ -0,0 +1,28 @@
+package items;
+
+public class items {
+	private String name;
+	private int ID;
+	private String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
